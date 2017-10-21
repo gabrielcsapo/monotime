2f878c643e55a638c10355fc0013e45cafe1f054
Gabriel Csapo <gabecsapo@gmail.com>
2017-10-21 02:07:15 -0700

    updates docs

--- tryitout.js
+++ tryitout.js
@@ -1,9 +1,10 @@
 module.exports = {
     title: "monotime",
     description: "💰 Money is time, a library to figure out what your time costs.",
-    source: "https://github.com/gabrielcsapo/monotime",
-    docs: "./code/index.html",
-    output: "./docs",
+    nav: {
+      Source: "https://github.com/gabrielcsapo/monotime",
+      Docs: "./code/index.html"
+    },
     body: [{
       type: "text",
       value: "> [`monotime`](https://github.com/gabrielcsapo/monotime) is an easy way to compute the amount of time a dollar amount will cost based on the normalized wage provided."
@@ -13,6 +14,7 @@ module.exports = {
       subtitle: "This will take an hourly wage and convert it into an object that you can query with a dollar amount to get cost in hours",
       value: `
         var monotime = require('monotime');
+
         monotime('hourly', 27.692307692307693).get('15'); // this will return how much time $15 dollars will cost you in terms hours
       `
     },{
@@ -24,12 +26,14 @@ module.exports = {
       subtitle: "This will take an time based wage and convert it into an object that you can query with a dollar amount to get cost in hours",
       value: `
         var monotime = require('monotime');
+
         console.log(monotime('hourly', 27.692307692307693).get('15'));
         console.log(monotime('bi-weekly', 2215.3846153846152).get('15'));
         console.log(monotime('monthly', 4800).get('15'));
         console.log(monotime('yearly', 57600).get('15'));
       `
     }],
+    output: "./docs",
     externals: [
       "./dist/monotime.js"
     ]
